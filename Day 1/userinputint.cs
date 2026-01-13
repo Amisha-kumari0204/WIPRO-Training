dad5833a914cd2643fe2420ebed7c7b0fedef50f
Amisha-Kumari_Keyjavi <Amisha_Kumari@keyjavi.com>
2026-01-13 23:36:10 +0530

    user input

---
+++ Day 1/userinputint.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello Amisha pls enter a number");
+        int a;
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your number is: " + a);
+    }
+}
