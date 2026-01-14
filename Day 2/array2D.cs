05c0dea75f07bf5676734b0cb1015034ce07ef5a
Amisha-kumari0204 <info.amisha02@gmail.com>
2026-01-14 20:08:50 +0530

    practice array

--- Day 2/array2D.cs
+++ Day 2/array2D.cs
@@ -4,28 +4,44 @@ class TwoDArrayProgram
 {
     static void Main()
     {
-        int[,] matrix = new int[3, 3]; 
+        // int[,] matrix = new int[3, 3]; 
 
-        matrix[0, 0] = 1;
-        matrix[0, 1] = 2;
-        matrix[0, 2] = 3;
+        // matrix[0, 0] = 1;
+        // matrix[0, 1] = 2;
+        // matrix[0, 2] = 3;
 
-        matrix[1, 0] = 4;
-        matrix[1, 1] = 5;
-        matrix[1, 2] = 6;
+        // matrix[1, 0] = 4;
+        // matrix[1, 1] = 5;
+        // matrix[1, 2] = 6;
 
-        matrix[2, 0] = 7;
-        matrix[2, 1] = 8;
-        matrix[2, 2] = 9;
-
-        Console.WriteLine("2D Array (Matrix):");
-        for (int i = 0; i < 3; i++) //Iterate through rows
+        // matrix[2, 0] = 7;
+        // matrix[2, 1] = 8;
+        // matrix[2, 2] = 9;
+        // Console.WriteLine("2D Array (Matrix):");
+        // for (int i = 0; i < 3; i++) //Iterate through rows
+        // {
+        //     for (int j = 0; j < 3; j++)
+        //     {
+        //         Console.Write(matrix[i, j] + " "); 
+        //     }
+        //     Console.WriteLine(); 
+        // }
+        int[,] a=new int[2,2];
+        for(int i=0;i<2;i++)
         {
-            for (int j = 0; j < 3; j++)
+            for(int j=0;j<2;j++)
             {
-                Console.Write(matrix[i, j] + " "); 
+                a[i,j]=Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine(); 
+        }
+
+        Console.WriteLine("The elements in the 2D array are:");
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                Console.WriteLine("Element at position "+i+","+j+" is: "+a[i,j]);
+            }
         }
 
      }
