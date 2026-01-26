234a17c98d91166affb3d288b396c93bac7fc41c
Amisha-kumari0204 <info.amisha02@gmail.com>
2026-01-26 20:45:12 +0530

    feat(date fun): date function exercise

--- Day 11_sql_basic/datefun.sql
+++ Day 11_sql_basic/datefun.sql
@@ -1,5 +1,6 @@
 USE TEST1;
 SELECT DB_NAME();
+select * from employees;
 
 --DATE FUNCTION GETDATE()
 SELECT GETDATE();
@@ -14,3 +15,24 @@ SELECT DATEDIFF(MONTH,2026-09-23,GETDATE());
 SELECT MONTH(GETDATE());
 SELECT DAY(GETDATE());
 SELECT YEAR(GETDATE());
+
+--formats like 'MM/DD/YYYY' or 'DD-Mon-YYYY
+SELECT FORMAT(GETDATE(),'MM-dd-yyyy');
+
+
+-----------------exercise----------------------
+--Find out each employee's 5-year anniversary date.
+SELECT emp_id,hire_date,DATEADD(year,5,hire_date) as anniv from employees;
+
+--Find employees hired in March.
+SELECT * FROM employees where MONTH(hire_date)=3;
+
+--Show the year, month, and day each employee was hired, separately.
+SELECT emp_id, year(hire_date) as yearOfHire,month(hire_date) as monthOfHire, day(hire_date)  as dayOfHire from employees;
+
+--Display the hire date in a standard US format (MM/dd/yyyy)
+SELECT FORMAT(GETDATE(),'MM/dd/yyyy');
+
+
+
+
